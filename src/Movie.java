5544dca5655e0628a72e00aab8ff44c0e5933f78
Martin Gondermann <magicmonty@pagansoft.de>
2012-06-24 14:11:36 +0200

    switched source of master to content of original tag

--- src/Movie.java
+++ src/Movie.java
@@ -1,18 +1,29 @@
 
 
-public abstract class Movie
+public class Movie
 {
+	public static final int CHILDRENS	= 2;
+	public static final int REGULAR 	= 0;
+	public static final int NEW_RELEASE = 1;
+	
 	private String title;
-
-	public Movie(String title) {
+	private int priceCode;
+	
+	public Movie (String title, int priceCode) {
 		this.title 		= title;
+		this.priceCode 	= priceCode;
 	}
-		
+	
+	public int getPriceCode () {
+		return priceCode;
+	}
+	
+	public void setPriceCode (int code) {
+		priceCode = code;
+	}
+	
 	public String getTitle () {
 		return title;
 	}
-
-  public abstract double determineAmount(int daysRented);
-
-  public abstract int determineFrequentRenterPoints(int daysRented);
+	
 }
